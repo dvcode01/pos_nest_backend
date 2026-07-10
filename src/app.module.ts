3ca00c949083f6545f28f817cec8b3bdd396b7c6
David <daviolivero2103@gmail.com>
2026-07-09 20:14:12 -0400

    chore: remove categories directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
+
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
