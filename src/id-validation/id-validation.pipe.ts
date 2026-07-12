6472add46349b3e7423718e4171e094c963e769e
David <daviolivero2103@gmail.com>
2026-07-11 22:27:37 -0400

    feat(categories): add numeric id validation pipe

---
+++ src/id-validation/id-validation.pipe.ts
@@ -0,0 +1,10 @@
+import { ArgumentMetadata, BadRequestException, Injectable, ParseIntPipe } from '@nestjs/common';
+
+@Injectable()
+export class IdValidationPipe extends ParseIntPipe {
+  constructor(){
+    super({
+      exceptionFactory: () => new BadRequestException('Invalid ID')
+    });
+  }
+}
