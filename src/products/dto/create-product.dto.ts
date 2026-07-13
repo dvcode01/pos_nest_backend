b2049153dcbeaf3befa3c92eff426429f1fec317
David <daviolivero2103@gmail.com>
2026-07-12 22:49:47 -0400

    chore(products): add validation in dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreateProductDto {
+    @IsNotEmpty({message: 'The product name is required'})
+    name: string;
+
+    @IsNotEmpty({message: 'The product price is required'})
+    @IsNumber({maxDecimalPlaces: 2}, {message: 'Invalid price'})
+    price: number;
+
+    @IsNotEmpty({message: 'The quantity cannot be empty'})
+    @IsNumber({maxDecimalPlaces: 0}, {message: 'Invalid value'})
+    inventory: number;
+
+    @IsNotEmpty({message: 'The category is required'})
+    @IsInt({message: 'The category is not valid'})
+    categoryId: number;
+}
