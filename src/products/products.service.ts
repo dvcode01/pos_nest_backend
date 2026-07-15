d40840543576f9122ef345cb74165ea5c614021e
David <daviolivero2103@gmail.com>
2026-07-15 13:41:29 -0400

    feat(products): delete product by id

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -85,7 +85,10 @@ export class ProductsService {
     return this.productRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const product = await this.findOne(id);
+    await this.productRepository.delete(product);
+
+    return 'Product eliminated';
   }
 }
