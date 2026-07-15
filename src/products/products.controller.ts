bf2c35191890f47983f5dc4cb405297d6ba71fc1
David <daviolivero2103@gmail.com>
2026-07-14 23:11:49 -0400

    chore(products): move dto to directory products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestj
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { GetProductsQueryDto } from 'src/categories/dto/get-product.dto';
+import { GetProductsQueryDto } from 'src/products/dto/get-product.dto';
 
 
 @Controller('products')
