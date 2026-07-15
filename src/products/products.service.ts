6a60839493cc8d1d7d3c817766d6a51bccefe2ff
David <daviolivero2103@gmail.com>
2026-07-14 22:45:36 -0400

    refactor(products): remove duplicate code in product retrieval logic

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { Category } from 'src/categories/entities/category.entity';
 
 @Injectable()
@@ -30,34 +30,24 @@ export class ProductsService {
   }
 
   async findAll(categoryId: number | null) {
-
-    if(categoryId){
-      const [products, total] = await this.productRepository.findAndCount({
-        where: {
-          category: {
-            id: categoryId
-          }
-        },
-        relations: {
+    const options: FindManyOptions<Product> = {
+      relations: {
           category: true
         },
         order: {
           id: 'DESC'
-        },
-      });
+        }
+    };
 
-      return {products, total};
+    if(categoryId){
+      options.where = {
+        category: {
+          id: categoryId
+        }
+      }
     }
 
-    const [products, total] = await this.productRepository.findAndCount({
-      relations: {
-        category: true
-      },
-      order: {
-        id: 'DESC'
-      } 
-    });
-
+    const [products, total] = await this.productRepository.findAndCount(options);
     return {products, total};
   }
 
