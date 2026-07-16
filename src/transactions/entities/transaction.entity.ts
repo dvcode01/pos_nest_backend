40399dcc53fe59b4e7d01458cca4b40fc37f6833
David <daviolivero2103@gmail.com>
2026-07-16 19:19:53 -0400

    feat(transactions): add transaction entities

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,36 @@
-export class Transaction {}
+import { Product } from "src/products/entities/product.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Transaction {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'decimal'})
+    total: number;
+
+    @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)'})
+    transactionDate: Date;
+
+    @OneToMany(() => TransactionContents, (transaction) => transaction.transaction)
+    contents: Transaction[];
+}
+
+@Entity()
+export class TransactionContents {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'int'})
+    quantity: number;
+
+    @Column({type: 'decimal'})
+    price: number;
+
+    @ManyToOne(() => Product, (product) => product.id, {eager: true})
+    product: Product;
+
+    @ManyToOne(() => Transaction, (transaction) => transaction.contents)
+    transaction: Transaction;
+}
+
