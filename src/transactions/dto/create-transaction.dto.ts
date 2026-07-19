dc482ff2822636e80cfa6c8118572f92b52d7364
David <daviolivero2103@gmail.com>
2026-07-18 22:24:35 -0400

    chore(transactions): add validation in dto

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,29 @@
-export class CreateTransactionDto {}
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsArray, IsInt, IsNotEmpty, IsNumber, IsOptional, Length, ValidateNested } from "class-validator";
+
+
+export class TransactionContentsDto {
+    @IsNotEmpty({ message: 'Product ID cannot be empty' })
+    @IsInt({ message: 'Invalid product' })
+    productId: number;
+
+    @IsNotEmpty({ message: 'Quantity cannot be empty' })
+    @IsInt({ message: 'Invalid quantity' }) // Validate quantity too
+    quantity: number;
+
+    @IsNotEmpty({ message: 'Price cannot be empty' })
+    @IsNumber({}, { message: 'Invalid price' })
+    price: number;
+}
+
+export class CreateTransactionDto {
+    @IsNotEmpty({ message: 'The total cannot be empty' })
+    @IsNumber({}, { message: 'Invalid quantity' })
+    total: number
+
+    @IsArray()
+    @ArrayNotEmpty({ message: 'The contents cannot be empty' })
+    @ValidateNested()
+    @Type(() => TransactionContentsDto)
+    contents: TransactionContentsDto[]
+}
