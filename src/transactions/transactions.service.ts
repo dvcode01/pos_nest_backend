e75146c3345303970030d1c3bd878c336e6274f2
David <daviolivero2103@gmail.com>
2026-07-18 23:15:07 -0400

    feat(transactions): implement sale data persistence

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction, TransactionContents } from './entities/transaction.entity';
+import { Product } from 'src/products/entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,
+    @InjectRepository(TransactionContents) private readonly transactionContentsRepository: Repository<TransactionContents>,
+    @InjectRepository(Product) private readonly productRepository: Repository<Product>
+  ){}
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    const transaction = new Transaction();
+    transaction.total = createTransactionDto.total;
+    await this.transactionRepository.save(transaction);
+    
+    for(const contents of createTransactionDto.contents){
+      const product = await this.productRepository.findOneBy({id: contents.productId});
+
+      if(!product){
+        throw new NotFoundException('Product not found');
+      }
+
+      if(product.inventory < contents.quantity){
+        throw new BadRequestException('Invalid quantity');
+      }
+
+      product.inventory -= contents.quantity;  
+      await this.transactionContentsRepository.save({...contents, transaction, product});
+    }
+
+    return 'Sale stored correctly';
   }
 
   findAll() {
