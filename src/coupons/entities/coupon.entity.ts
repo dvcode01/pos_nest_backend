08a77da29212afb7f486131132aa3e1b873a273e
David <daviolivero2103@gmail.com>
2026-07-20 22:31:19 -0400

    feat(coupons): add fields in entity

--- src/coupons/entities/coupon.entity.ts
+++ src/coupons/entities/coupon.entity.ts
@@ -1,1 +1,16 @@
-export class Coupon {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Coupon {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'varchar', length: 35})
+    name: string;
+
+    @Column({type: 'int'})
+    percentage: number;
+
+    @Column({type: 'date'})
+    expirationDate: Date;
+}
