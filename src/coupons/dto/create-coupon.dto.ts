f3cca648a5f06ebe0207b03cd29c5f433dfbf884
David <daviolivero2103@gmail.com>
2026-07-20 22:32:05 -0400

    chore(coupons): add validation in dto fields

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, IsString, Max, Min } from "class-validator";
+
+export class CreateCouponDto {
+    @IsNotEmpty({message: 'The coupon name is required'})
+    @IsString({message: 'Invalid name'})
+    name: string;
+
+    @IsNotEmpty({message: 'The discount cannot be empty'})
+    @IsInt({message: 'The discount must be between 1 and 100'})
+    @Max(100, {message: 'The maximum discount is 100'})
+    @Min(1, {message: 'The minimum discount is 1'})
+    percentage: number;
+
+    @IsNotEmpty({message: 'The date field cannot be empty'})
+    @IsDateString({}, {message: 'Invalid date'})
+    expirationDate: Date;
+}
