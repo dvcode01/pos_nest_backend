21fa4ec0aa7c17f6982873ff5737dce3185de41f
David <daviolivero2103@gmail.com>
2026-07-25 21:41:44 -0400

    feat(seeder): registering repositories and injecting Data

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -3,6 +3,8 @@ import { SeederService } from './seeder.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from '../config/typeorm.config';
+import { Category } from '../categories/entities/category.entity';
+import { Product } from '../products/entities/product.entity';
 
 @Module({
   imports: [
@@ -13,6 +15,7 @@ import { typeORMConfig } from '../config/typeorm.config';
         useFactory: typeORMConfig,
         inject: [ConfigService]
       }), 
+      TypeOrmModule.forFeature([Category, Product])
     ],
   providers: [SeederService]
 })
--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
+import { products } from './data/products';
+import { categories } from './data/categories';
 
 @Injectable()
 export class SeederService {
-    async seed(){
+    constructor(
+        @InjectRepository(Category) private readonly categoryRepository: Repository<Category>,
+        @InjectRepository(Product) private readonly productRepository: Repository<Product>,
+    ) { }
+
+    async seed() {
+        await this.categoryRepository.save(categories);
+
+        for await (const seedProduct of products) {
+            const category = await this.categoryRepository.findOneBy({ id: seedProduct.categoryId });
+            const product = new Product();
+
+            if (!category) {
+                throw new Error(`Category with ID ${seedProduct.categoryId} not found`);
+            }
+
+            product.name = seedProduct.name;
+            product.image = seedProduct.image;
+            product.price = seedProduct.price;
+            product.inventory = seedProduct.inventory;
+            product.category = category;
+            await this.productRepository.save(product);
+        }
+
         console.log('Desde seed');
     }
 
