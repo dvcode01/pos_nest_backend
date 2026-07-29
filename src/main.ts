1c082b5003e614f535b6c357df4e69e3f3fd48c7
David <daviolivero2103@gmail.com>
2026-07-28 21:11:23 -0400

    chore: change number port

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   }));
 
   app.useStaticAssets(join(__dirname, '../public'));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
